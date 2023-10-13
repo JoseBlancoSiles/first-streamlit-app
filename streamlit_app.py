b92ff6ea0c6e75ab0b9ea8abe990cf3ef399a5d2
José Siles <107679686+JoseBlancoSiles@users.noreply.github.com>
2023-10-13 15:04:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Favorites')
@@ -42,11 +47,6 @@ except URLError as e:
   streamlit.error()
 
 streamlit.header("The fruit load list contains:")
-
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT * from fruit_load_list")
-        return my_cur.fetchall()
         
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
